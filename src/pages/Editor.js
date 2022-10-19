77cbd8c187e2e43a5ee1d7986cb929fb4f9deb7c
Ye sohyeon <93248242+yesohyeon@users.noreply.github.com>
2022-10-19 22:21:00 +0900

    feat: implement managing state in editor page and design

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,21 +1,46 @@
-import { Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 
+import CodeArea from "../components/editor/CodeArea";
+import FunctionHeader from "../components/editor/FunctionHeader/FunctionHeader";
+import LanguageBar from "../components/editor/LanguageBar";
+import ToolBar from "../components/editor/ToolBar";
+import { LIGHT_GREY_100 } from "../constants/color";
 import AppHeader from "../layout/AppHeader";
 import ContentBox from "../layout/ContentBox";
 import Layout from "../layout/Layout";
 
-function Editor() {
+export default function Editor() {
+  const [selectedLanguage, setSelectedLanguage] = useState("html");
+  const [code, setCode] = useState({
+    html: "",
+    css: "",
+    js: "",
+  });
+
+  const selectedLanguageCode = code[selectedLanguage];
+
   return (
     <Layout>
       <AppHeader>
-        <Text>FunctionHeader</Text>
+        <Feather name="menu" size={30} color={LIGHT_GREY_100} />
+        <FunctionHeader code={code} />
       </AppHeader>
       <ContentBox>
-        <Text>LanguageBar</Text>
-        <Text>CodeArea</Text>
+        <LanguageBar
+          selectedLanguage={selectedLanguage}
+          handlePress={(language) => setSelectedLanguage(language)}
+        />
+        <CodeArea code={selectedLanguageCode} />
+        <ToolBar
+          handleChange={(str) =>
+            setCode((prev) => ({
+              ...prev,
+              [selectedLanguage]: prev[selectedLanguage] + str,
+            }))
+          }
+        />
       </ContentBox>
     </Layout>
   );
 }
-
-export default Editor;
