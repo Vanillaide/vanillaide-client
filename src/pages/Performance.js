a74b3bc22151d5cb90b0bd587253744ae42352a3
Sohyeon <needtlc13@gmail.com>
2022-10-21 11:59:38 +0900

    feat: implement start and loading page of deploy and performance

--- src/pages/Performance.js
+++ src/pages/Performance.js
@@ -1,21 +1,60 @@
-import { Text, Button } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useState, useContext } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 
+import CustomButton from "../components/CustomButton";
 import Logo from "../components/Logo";
+import { LIGHT_GREY_100 } from "../constants/color";
+import { ProjectContext } from "../contexts/ProjectProvider";
 import AppHeader from "../layout/AppHeader";
 import ContentBox from "../layout/ContentBox";
 import Layout from "../layout/Layout";
 
 export default function Performance() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { focusedProject } = useContext(ProjectContext);
+
+  const handlePress = () => {
+    setIsLoading(true);
+  };
+
   return (
     <Layout>
       <AppHeader>
-        <Text>NavBarIcon</Text>
+        <Feather name="menu" size={30} color={LIGHT_GREY_100} />
         <Logo fontSize={30} />
+        <View />
       </AppHeader>
       <ContentBox>
-        <Logo fontSize={70} />
-        <Button title="measure performance" />
+        {!isLoading ? (
+          <>
+            <Text style={styles.projectName}>{focusedProject.projectName}</Text>
+            <CustomButton
+              text="start measuring"
+              fontSize={20}
+              handlePress={handlePress}
+              buttonWidth={250}
+              buttonHeight={50}
+              borderRadius={30}
+            />
+          </>
+        ) : (
+          <ActivityIndicator
+            animating={isLoading}
+            size="large"
+            color={LIGHT_GREY_100}
+          />
+        )}
       </ContentBox>
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  projectName: {
+    marginBottom: 10,
+    fontSize: 70,
+    fontFamily: "FiraCode",
+    color: LIGHT_GREY_100,
+  },
+});
