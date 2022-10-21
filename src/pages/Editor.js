21219f164062dec33640b28ba69105f3476002e6
taeuk <dcjimin@gmail.com>
2022-10-21 21:20:47 +0900

    merge

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,48 +1,32 @@
-// import { Feather } from "@expo/vector-icons";
-// import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  KeyboardAvoidingView as View,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import { WebView } from "react-native-webview";
 
-// import CodeArea from "../components/editor/CodeArea";
-// import FunctionHeader from "../components/editor/FunctionHeader/FunctionHeader";
-// import LanguageBar from "../components/editor/LanguageBar";
-// import ToolBar from "../components/editor/ToolBar";
-// import { LIGHT_GREY_100 } from "../constants/color";
-// import AppHeader from "../layout/AppHeader";
-// import ContentBox from "../layout/ContentBox";
-// import Layout from "../layout/Layout";
+const statusBarHeight = StatusBar.currentHeight;
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
 
 export default function Editor() {
-  // const [selectedLanguage, setSelectedLanguage] = useState("html");
-  // const [code, setCode] = useState({
-  //   html: "",
-  //   css: "",
-  //   js: "",
-  // });
+  const [dimensions, setDimensions] = useState({ window, screen });
+  console.log(dimensions.window, dimensions.screen);
 
-  // const selectedLanguageCode = code[selectedLanguage];
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setDimensions({ window, screen });
+      },
+    );
+    return () => subscription?.remove();
+  });
 
   return (
-    <WebView source={{ uri: process.env.REACT_APP_REACT_URI }} />
-    // <Layout>
-    //   <AppHeader>
-    //     <Feather name="menu" size={30} color={LIGHT_GREY_100} />
-    //     <FunctionHeader code={code} />
-    //   </AppHeader>
-    //   <ContentBox>
-    //     <LanguageBar
-    //       selectedLanguage={selectedLanguage}
-    //       handlePress={(language) => setSelectedLanguage(language)}
-    //     />
-    //     <CodeArea code={selectedLanguageCode} />
-    //     <ToolBar
-    //       handleChange={(str) =>
-    //         setCode((prev) => ({
-    //           ...prev,
-    //           [selectedLanguage]: prev[selectedLanguage] + str,
-    //         }))
-    //       }
-    //     />
-    //   </ContentBox>
-    // </Layout>
+    <View style={{ marginTop: statusBarHeight, flex: 1 }}>
+      <WebView source={{ uri: "http://192.168.0.50:3000" }} />
+    </View>
   );
 }
